a51cc9709aab82c8361a36e9f863ffa182003fa2
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2018-02-07 18:25:05 +0100

    fix images

--- js/pages/home/home-controller.js
+++ js/pages/home/home-controller.js
@@ -1,20 +1,12 @@
 app.controller('HomeController', function($scope, $rootScope, $http, $routeParams, config, ParallaxService) {  
 
-
+    ParallaxService.add($('section#home-1'), true);
+    ParallaxService.add($('section#home-4'), true);
     // $("section#home-1").paroller({ factor: '0.75'});
     // $("section#home-4").paroller({ factor: '0.75'});
-
-
-    // setTimeout(function(){
-    	
-	    ParallaxService.add($('section#home-1'), true);
-	    ParallaxService.add($('section#home-4'), true);
-
-	    $('section#home-3 .first-column, section#home-4 .lead h2').appear(function() {
-	     	$(this).addClass('appeared');
-	    });
-
-	// }, 0);
-
+    
+    $('section#home-3 .first-column, section#home-4 .lead h2').appear(function() {
+     	$(this).addClass('appeared');
+    });
 
 });
